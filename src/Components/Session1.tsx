d5e57872a19e6640f83800854bcf124ff0be54f6
Prathik2 <prathikrajesh02@gmail.com>
2025-02-21 07:49:31 +0530

    Session2 changes
    Signed-off-by: Prathik2 <prathikrajesh02@gmail.com>

--- src/Components/Session1.tsx
+++ src/Components/Session1.tsx
@@ -5,7 +5,7 @@ export const Session1: React.FC = () => {
     const [count, setCount] = useState<number>(0);
     const [input, setInput] = useState<number>();
     // Here if there is no useMemo, then squareNumber() will not execute whenever the count gets incremented.
-    const squaredNumber = useMemo(() => squareNumber(input),[input])
+    const squaredNumber: number = useMemo(() => squareNumber(input),[input])
 
     const handleIncrementClick = () => {
         setCount(count + 1);
@@ -20,7 +20,7 @@ export const Session1: React.FC = () => {
             <div style={{color: "white"}}>Count: {count}</div>
             <div><button onClick={handleIncrementClick}>Increment</button></div>
             <div style={{color: "white"}}><label>Give number</label><input type="number" value={input} onChange={setInputValue} /></div>
-            <p style={{color: "white"}}>Squared Value: {squaredNumber}</p>
+            <p style={{color: "white"}}>Squared Value: {squaredNumber === undefined || 0  ? 0 : squaredNumber}</p>
         </div>
     )
 }
